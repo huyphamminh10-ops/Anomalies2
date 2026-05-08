5b0053fa77def1a45b7c369e1ca7b9cb4e18eb01
Nang5Gram <huyphamminh10@gmail.com>
2026-05-09 05:41:27 +0700

    99

--- dashboard/server.py
+++ dashboard/server.py
@@ -102,7 +102,11 @@ app = FastAPI(title="Anomalies Dashboard")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://anomalies2.onrender.com",
+        "http://localhost:8000",
+        "http://127.0.0.1:8000",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
